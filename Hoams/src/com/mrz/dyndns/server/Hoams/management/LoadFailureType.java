9d236447b187546cd8c6dc70ba2abf1efd966c2b
Zora <MrZoraman@Gmail.com>
2014-01-25 20:40:27 -0800

    A little bit of documentation

--- Hoams/src/com/mrz/dyndns/server/Hoams/management/LoadFailureType.java
+++ Hoams/src/com/mrz/dyndns/server/Hoams/management/LoadFailureType.java
@@ -2,7 +2,18 @@ package com.mrz.dyndns.server.Hoams.management;
 
 public enum LoadFailureType 
 {
+	/**
+	 * Everything went according to plan :)
+	 */
 	NONE,
+	
+	/**
+	 * The map the home is on is not loaded, or doesn't exist (therefore, not loaded!)
+	 */
 	NO_MAP,
-	NO_HOME
+	
+	/**
+	 * The player doesn't have his/her home set
+	 */
+	NO_HOME;
 }
